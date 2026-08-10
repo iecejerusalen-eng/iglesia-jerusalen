aefa1aec9da83051223e58f42a9fd8dbbad0dd12
Administrador <admin@iglesiajerusalen.com>
2026-08-10 18:26:54 -0500

    chore: clean up test imports

--- src/components/common/toolbox/__tests__/GlobalToolbox.test.tsx
+++ src/components/common/toolbox/__tests__/GlobalToolbox.test.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { afterEach, beforeEach, describe, expect, it } from 'vitest';
 import GlobalToolbox from '../../GlobalToolbox';
